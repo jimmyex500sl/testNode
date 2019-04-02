f9843295abaed564e33cf530fa3c0f3192fc7e27
jimmyex500sl <jimmy.yang@autonet.com.tw>
2019-04-02 08:26:40 +0800

    testing

--- main.js
+++ main.js
@@ -26,29 +26,46 @@ var arduinoSerialPort = new SerialPort(arduinoCOMPort, {
  baudBate: 9600
 });
 
-arduinoSerialPort.on('open',function() {
+arduinoSerialPort.on('open',function(data) {
   console.log('Serial Port ' + arduinoCOMPort + ' is opened.');
+    console.log(data);
 });
 
 app.get('/', function (req, res) {
 
     res.send('<h1>Working on express<\h1>');
  
-})
+});
+
+arduinoSerialPort.on('data', function (data) {
+  console.log(data.toString('utf8'));
+});
+
+app.get('rpm',function(req, res) {
+
+  var action = req.params.action || req.param('rpm');
+
+    console.log(action);
+
+});
 
 app.get('/:action', function (req, res) {
     
-   var action = req.params.action || req.param('action');
+   var action = req.params.action ;
     
-    if(action == 'led'){
-        arduinoSerialPort.write("o");
-        return res.send('Led light is on!');
-    } 
-    if(action == 'off') {
-        arduinoSerialPort.write("f");
-        return res.send("Led light is off!");
-    }
-    
+    // if(isNaN(action)){
+    //     arduinoSerialPort.write("rpm1000|");
+    //     return res.send('rpm:1000|');
+    // } 
+    // if(action == 'rpm2') {
+    //     arduinoSerialPort.write("rpm2000|");
+    //     return res.send("rpm:2000|");
+    // }
+
+    console.log(action);
+
+    arduinoSerialPort.write(action);
+
     return res.send('Action: ' + action);
  
 });
